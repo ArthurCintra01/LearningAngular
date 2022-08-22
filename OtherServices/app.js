44215aa6015c281f36c9d5817fe98538f0bb2270
Arthur Cintra <turcko.cintra@gmail.com>
2022-08-22 11:49:22 -0300

    other services

--- OtherServices/app.js
+++ OtherServices/app.js
@@ -1,17 +1,18 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages','ngResource']);
 
-myApp.controller('mainController', function($scope, $log , $filter) {
+myApp.controller('mainController', function($scope, $log , $filter, $resource) {
     // angular $log functions
-    $log.log("Hello");
-    $log.info("this is some information");
-    $log.warn("Warning!");
-    $log.debug("Some debug information");
-    $log.error("This is an error example!!!")
+    // $log.log("Hello");
+    // $log.info("this is some information");
+    // $log.warn("Warning!");
+    // $log.debug("Some debug information");
+    // $log.error("This is an error example!!!")
 
     // filter 
-    $scope.name = 'Arthur';
-    $scope.formattedname = $filter('uppercase')($scope.name);
+    // $scope.name = 'Arthur';
+    // $scope.formattedname = $filter('uppercase')($scope.name);
 
-    $log.info($scope.name);
-    $log.info($scope.formattedname);
+    // $log.info($scope.name);
+    // $log.info($scope.formattedname);
+    console.log($resource);
 });
