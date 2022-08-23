fb5021aed6b78bd970f3e8f4409cf5dd6e46ac02
Arthur Cintra <turcko.cintra@gmail.com>
2022-08-23 20:24:43 -0300

    trying api

--- BuildingApp/app.js
+++ BuildingApp/app.js
@@ -28,12 +28,27 @@ myApp.controller('homeController', ['$scope','cityService', function($scope, cit
 }]);
 
 // forecast controller
-myApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
+myApp.controller('forecastController', ['$scope', '$resource', 'cityService','$http', function($scope, $resource, cityService,$http) {
     $scope.city = cityService.city;
 
-    $scope.weatherAPI = $resource("http://api.weatherapi.com/v1/forecast.json?key=b403b91067474db092e7868468bcbc1a", { callback: "JSON_CALLBACK" },+'&'+ { get: { method: "JSONP" }});
+    var URL = 'http://api.openweathermap.org/data/2.5/weather?';
+  
+    var request = {
+        method: 'GET',
+        url: URL,
+        params: {
+            q: $scope.city,
+            mode: 'json',
+            units: 'metric',
+            cnt: '7',
+            appid: '9ad487b9ac421b539ebb3e0aa76845ee'
+        }
+    };
+    $scope.weatherAPI = $http(request);
 
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city});
+    // $scope.weatherAPI = $resource("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e816dd74aec237843db49bcaea4fc0bf", { callback: "JSON_CALLBACK" },+'&'+ { get: { method: "JSONP" }});
+
+    //$scope.weatherResult = $scope.weatherAPI.get();
 
     console.log($scope.weatherResult);
 }]);
